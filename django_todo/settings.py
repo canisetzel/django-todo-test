5c315749b2a23f39a2533d06c20823d95ff01a48
Attila Nagy <attila.nagy@coincash.eu>
2022-07-28 16:11:45 +0200

    db test 1

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo',
+        'USER': 'an',
+        'PASSWORD': 'an',
+        'HOST': '10.131.3.15',
+        'PORT': '5432',
     }
 }
 
